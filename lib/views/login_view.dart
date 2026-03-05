390f554a977e9884908ca21fa86d08e1967710d2
JustKyo12 <tokowahyu51@gmail.com>
2026-03-05 23:56:21 +0700

    Login View

---
+++ lib/views/login_view.dart
@@ -0,0 +1,87 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  // 1. Deklarasi TextEditingController untuk menangkap inputan teks
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  // 2. Inisialisasi controller
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  // 3. Hapus controller dari memori saat halaman ditutup
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Column(
+        children: [
+          // TextField untuk Email
+          TextField(
+            controller: _email,
+            keyboardType: TextInputType.emailAddress,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(
+              hintText: 'Enter your email here',
+            ),
+          ),
+          // TextField untuk Password
+          TextField(
+            controller: _password,
+            obscureText: true, // Menyembunyikan teks password
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(
+              hintText: 'Enter your password here',
+            ),
+          ),
+          // Tombol Login
+          TextButton(
+            onPressed: () async {
+              // _email dan _password sekarang sudah dikenali
+              final email = _email.text;
+              final password = _password.text;
+              
+              try {
+                await FirebaseAuth.instance.signInWithEmailAndPassword(
+                  email: email,
+                  password: password,
+                );
+                print('Login sukses!');
+              } on FirebaseAuthException catch (e) {
+                if (e.code == 'user-not-found') {
+                  print('User not found');
+                } else if (e.code == 'wrong-password') {
+                  print('Wrong password');
+                }
+              }
+            },
+            child: const Text('Login'),
+          ),
+        ],
+      ),
+    );
+  }
+}
