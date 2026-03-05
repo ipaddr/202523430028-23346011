771cf244a13e5dc9baac70ea26844e8f8dbdda1e
JustKyo12 <tokowahyu51@gmail.com>
2026-03-06 00:17:34 +0700

    Link Between Login and Register Views

---
+++ lib/views/verify_email_view.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class VerifyEmailView extends StatefulWidget {
+  const VerifyEmailView({Key? key}) : super(key: key);
+
+  @override
+  State<VerifyEmailView> createState() => _VerifyEmailViewState();
+}
+
+class _VerifyEmailViewState extends State<VerifyEmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Verify email')),
+      body: Column(
+        children: [
+          const Text("We've sent you an email verification. Please open it to verify your account."),
+          const Text("If you haven't received the email, press the button below:"),
+          TextButton(
+            onPressed: () async {
+              final user = FirebaseAuth.instance.currentUser;
+              await user?.sendEmailVerification();
+            },
+            child: const Text('Send email verification'),
+          ),
+          TextButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              // Akan diarahkan kembali ke Register/Login
+            },
+            child: const Text('Restart'),
+          )
+        ],
+      ),
+    );
+  }
+}
